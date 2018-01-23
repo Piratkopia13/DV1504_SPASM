34a2254721db49b2e83b7857804c02758ab6914a
Emil <emilwahlgmail.com>
2018-01-23 15:37:59 +0100

    Added const to variables

--- Project/src/game/PlayerCameraController.cpp
+++ Project/src/game/PlayerCameraController.cpp
@@ -13,7 +13,7 @@ void PlayerCameraController::update(float dt) {
 	update(dt, Vector3(0.f, 0.f, 0.f));
 }
 
-void PlayerCameraController::update(float dt, Vector3 playerPos) {
+void PlayerCameraController::update(float dt, const Vector3& playerPos) {
 	
 	setCameraPosition(Vector3(playerPos.x, playerPos.y, cameraOffset));
 }
--- Project/src/game/PlayerCameraController.h
+++ Project/src/game/PlayerCameraController.h
@@ -5,7 +5,7 @@ public:
 	PlayerCameraController(Camera* cam);
 
 	virtual void update(float dt);//Probably won't need dt
-	void update(float dt, DirectX::SimpleMath::Vector3 playerPos);
+	void update(float dt, const DirectX::SimpleMath::Vector3& playerPos);
 
 private:
 
