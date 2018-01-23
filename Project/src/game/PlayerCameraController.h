1492b47591aab4ef0981d70b51aee1e66c498079
Emil <emilwahlgmail.com>
2018-01-23 15:23:08 +0100

    Adding new camera functionalites to the game state

--- Project/src/game/PlayerCameraController.h
+++ Project/src/game/PlayerCameraController.h
@@ -4,7 +4,7 @@ class PlayerCameraController : public CameraController {
 public:
 	PlayerCameraController(Camera* cam);
 
-	virtual void update(float dt);
+	virtual void update(float dt);//Probably won't need dt
 	void update(float dt, DirectX::SimpleMath::Vector3 playerPos);
 
 private:
