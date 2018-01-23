a53ed126346033ce2c3aac3b552f093b80c2e1ee
Alexander Wester <piratkopia13@gmail.com>
2018-01-23 14:13:21 +0100

    Added return values for GameState

--- Project/src/game/states/GameState.cpp
+++ Project/src/game/states/GameState.cpp
@@ -9,12 +9,15 @@ GameState::GameState(StateStack& stack)
 // Process input for the state
 bool GameState::processInput(float dt) {
 
+	return true;
 }
 // Updates the state
 bool GameState::update(float dt) {
 
+	return true;
 }
 // Renders the state
 bool GameState::render(float dt) {
 
+	return true;
 }
