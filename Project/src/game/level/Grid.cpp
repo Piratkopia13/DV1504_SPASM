0384e9b98d86092f3200ca843bc3e73ff194a46d
Emil <emilwahlgmail.com>
2018-01-25 16:37:57 +0100

    Added function to find indices for blocks in grid. Algorithm needs to be verified that it works.

--- Project/src/game/level/Grid.cpp
+++ Project/src/game/level/Grid.cpp
@@ -26,3 +26,60 @@ bool Grid::atGrid(const int x, const int y) {
 		return true;
 	return false;
 }
+
+std::vector<Grid::Index> Grid::getCollisionIndices(const AABB& boundingBox, const float gridSize) {
+	std::vector<Grid::Index> indices;
+	DirectX::SimpleMath::Vector2 bottomLeft;
+	DirectX::SimpleMath::Vector2 topRight;
+
+	//Calculating minimum and maximum indices in the grid that the bounding box is in
+	bottomLeft.x = boundingBox.getMinPos().x;
+	bottomLeft.y = boundingBox.getMinPos().y;
+	topRight.x = boundingBox.getMaxPos().x;
+	topRight.y = boundingBox.getMaxPos().y;
+
+	bottomLeft /= gridSize;
+	topRight /= gridSize;
+
+	int minX = static_cast<int>(floor(bottomLeft.x));
+	int minY = static_cast<int>(floor(bottomLeft.y));
+	int maxX = static_cast<int>(floor(topRight.x));
+	int maxY = static_cast<int>(floor(topRight.y));
+
+	//Getting all the boxes surrounding the bounding box
+	int top = maxY + 1;
+	int bottom = minY - 1;
+	for (int i = minX - 1; i < maxX+1; i++) {
+		if (m_cells[i][bottom] != nullptr) {
+			Grid::index index;
+			index.x = i;
+			index.y = bottom;
+			indices.push_back(index);
+		}
+		if (m_cells[i][top] != nullptr) {
+			Grid::index index;
+			index.x = i;
+			index.y = top;
+			indices.push_back(index);
+		}
+	}
+	top = maxX + 1;
+	bottom = minX - 1;
+	for (int i = minY; i < maxY; i++) {
+		if (m_cells[bottom][i] != nullptr) {
+			Grid::index index;
+			index.x = bottom;
+			index.y = i;
+			indices.push_back(index);
+		}
+		if (m_cells[top][i] != nullptr) {
+			Grid::index index;
+			index.x = top;
+			index.y = i;
+			indices.push_back(index);
+		}
+	}
+
+
+	return indices;
+}
--- Project/src/game/level/Grid.h
+++ Project/src/game/level/Grid.h
@@ -2,9 +2,16 @@
 
 #include "../../sail/Sail.h"
 
+
 class Block;
 class Grid {
 public:
+
+	struct Index {
+		int x;
+		int y;
+	};
+
 	Grid();
 	Grid(const int worldWidth, const int worldHeight);
 	~Grid();
@@ -12,6 +19,7 @@ public:
 	
 	void addBlock(Block* block, const int x, const int y);
 	bool atGrid(const int x, const int y);
+	std::vector<Index> getCollisionIndices(const AABB& boundingBox, const float gridSize);
 
 private:
 	std::vector<std::vector<Block*>> m_cells;
