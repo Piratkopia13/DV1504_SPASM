ccc7fd5a6a8087c4614b5943a53a7e47d6e15560
H3nx <h3nx@hotmail.se>
2018-01-29 12:09:11 +0100

    removed gravity

--- Project/src/game/objects/Projectile.cpp
+++ Project/src/game/objects/Projectile.cpp
@@ -1,6 +1,6 @@
 #include "Projectile.h"
 
-Projectile::Projectile(const DirectX::SimpleMath::Vector3& position, DirectX::SimpleMath::Vector3 velocity, float damage, int team, float mass) : Moveable(mass) {
+Projectile::Projectile(const DirectX::SimpleMath::Vector3& position, DirectX::SimpleMath::Vector3 velocity, float damage, int team, float mass) {
 	setVelocity(DirectX::SimpleMath::Vector3(velocity));
 	setPosition(DirectX::SimpleMath::Vector3(position));
 	m_damage = damage;
