700ff17c0dec2ff510c85645d8cdf5952891be41
H3nx <h3nx@hotmail.se>
2018-02-05 21:02:35 +0100

    Fixed namespace

--- Project/src/game/PlayerCameraController.h
+++ Project/src/game/PlayerCameraController.h
@@ -2,23 +2,21 @@
 #include "../sail/Sail.h"
 #include "objects\Character.h"
 
-using namespace DirectX;
-using namespace DirectX::SimpleMath;
 class PlayerCameraController : public CameraController {
 public:
 	PlayerCameraController(Camera* cam, const DirectX::SimpleMath::Vector2* mapSize = nullptr);
 
 	void update(float dt);
 	void setTargets(Object* focusObject1, Object* focusObject2 = nullptr, Object* focusObject3 = nullptr, Object* focusObject4 = nullptr);
-	void setPosition(Vector3 pos);
-	void setTarget(Vector3 pos);
-	void setOffset(Vector3 offset);
+	void setPosition(DirectX::SimpleMath::Vector3 pos);
+	void setTarget(DirectX::SimpleMath::Vector3 pos);
+	void setOffset(DirectX::SimpleMath::Vector3 offset);
 	void setMoveSpeed(float speed);
 	void setFollowSpeed(float speed);
 
 
-	Vector3 getPosition();
-	Vector3 getTarget();
+	DirectX::SimpleMath::Vector3 getPosition();
+	DirectX::SimpleMath::Vector3 getTarget();
 
 	void setMoving(bool stat);
 	bool useExtra;
@@ -27,12 +25,12 @@ private:
 	Object * targets[4];
 
 	
-	Vector3 position;
-	Vector3 target;
+	DirectX::SimpleMath::Vector3 position;
+	DirectX::SimpleMath::Vector3 target;
 
 
-	Vector3 back;
-	Vector3 up;
+	DirectX::SimpleMath::Vector3 back;
+	DirectX::SimpleMath::Vector3 up;
 
 	bool m_lockToMap;
 	DirectX::SimpleMath::Vector2 m_mapSize;
