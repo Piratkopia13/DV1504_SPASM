74fcfcc3afb52e3650515526da81c1dd45f136a1
Alexander Wester <piratkopia13@gmail.com>
2018-02-09 11:49:50 +0100

    Tweaked infplanes color shift

--- Project/src/game/states/GameState.cpp
+++ Project/src/game/states/GameState.cpp
@@ -165,7 +165,7 @@ bool GameState::update(float dt) {
 	static float epilepsySpeed = 0.3f;
 	static float counter = 0;
 	counter += dt * epilepsySpeed;
-	Vector4 infColor(-fabs(sinf(counter)) * epilepsyAmount, -fabs(cos(counter)) * epilepsyAmount, -fabs(sinf(counter) + 0.5f) * epilepsyAmount, 1.f);
+	Vector4 infColor(-fabs(sinf(counter)) * epilepsyAmount, -fabs(sin(counter + 2.f)) * epilepsyAmount, -fabs(sinf(counter + 4.f)) * epilepsyAmount, 1.f);
 	m_infBottom->setColor(infColor);
 	m_infTop->setColor(infColor);
 	m_infLeft->setColor(infColor);
