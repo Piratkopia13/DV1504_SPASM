f5c789773303b886f156368d234d09ae0661a47c
Fredrik <fredrik.junede@gmail.com>
2018-02-22 10:43:19 +0100

    Added Gamemode update to Deathmatch update

--- Project/src/game/gamemodes/deathmatch/Deathmatch.cpp
+++ Project/src/game/gamemodes/deathmatch/Deathmatch.cpp
@@ -13,11 +13,14 @@ Deathmatch::~Deathmatch() {
 
 
 void Deathmatch::update(CharacterHandler* charHandler, float delta) {
+	Gamemode::update(charHandler, delta);
 	
 	unsigned int numOfPlayers = charHandler->getNrOfPlayers();
 	for (unsigned int i = 0; i < numOfPlayers; i++) {
 		Character* character = charHandler->getCharacter(i);
 	}
+
+
 }
 
 
@@ -27,5 +30,9 @@ void Deathmatch::draw() {
 
 
 int Deathmatch::checkWin() {
+	// Get kills
+	// if kills exceeds x
+	// win
+
 	return 0;
 }
