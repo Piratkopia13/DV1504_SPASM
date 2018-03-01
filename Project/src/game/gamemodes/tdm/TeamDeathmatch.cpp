5813ec738faf4817129e74bd66713734d84f2878
Fredrik <fredrik.junede@gmail.com>
2018-03-01 08:31:51 +0100

    TeamDeathmatch should now work

--- Project/src/game/gamemodes/tdm/TeamDeathmatch.cpp
+++ Project/src/game/gamemodes/tdm/TeamDeathmatch.cpp
@@ -17,12 +17,30 @@ void TeamDeathmatch::update(CharacterHandler* charHandler, float delta) {
 	Gamemode::update(charHandler, delta);
 
 	unsigned int numOfPlayers = charHandler->getNrOfPlayers();
+	int teamOne = 0;
+	int teamTwo = 0;
 	Character* character = nullptr;
 	for (unsigned int i = 0; i < numOfPlayers; i++) {
-		if (GameInfo::getInstance()->getScore().getPlayerStats(i).kills > m_maxKills)
-			m_winnerID = i;
+		switch (charHandler->getCharacter(i)->getTeam()) {
+		case 0:
+			teamOne = GameInfo::getInstance()->getScore().getPlayerStats(i).kills;
+			break;
+		case 1:
+			teamTwo = GameInfo::getInstance()->getScore().getPlayerStats(i).kills;
+			break;
+		default:
+			Logger::Warning("The number of teams are too many");
+			return;
+		}
 	}
 
+
+	if (teamOne >= m_maxKills)
+		m_winnerID = 0;
+
+
+	if (teamTwo >= m_maxKills)
+		m_winnerID = 1;
 
 }
 
