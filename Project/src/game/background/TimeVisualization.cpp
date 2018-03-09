7db147166451c3d0b481d4ea1eae11e8bb57546b
Fredrik <fredrik.junede@gmail.com>
2018-03-09 17:02:26 +0100

    Updated the clock animation

--- Project/src/game/background/TimeVisualization.cpp
+++ Project/src/game/background/TimeVisualization.cpp
@@ -11,11 +11,13 @@ TimeVisualization::TimeVisualization(Level* level, Gamemode* gamemode) {
 	m_gamemode = gamemode;
 
 	m_timeLeft = gamemode->getGametime();
-	m_timeBlocks.resize(int(m_timeLeft / 1.5f));
-	m_toRemove.resize(m_timeBlocks.size());
 	m_run = false;
 
 	if (m_timeLeft < 60000000.f) {
+
+		m_timeBlocks.resize(120);
+		m_toRemove.resize(m_timeBlocks.size());
+
 		m_blockModel = Application::getInstance()->getResourceManager().getFBXModel("block").getModel();
 
 		m_numOfBlocks = m_timeBlocks.size();
@@ -28,19 +30,20 @@ TimeVisualization::TimeVisualization(Level* level, Gamemode* gamemode) {
 
 		float paj = PI / (float(m_timeBlocks.size()) / 2.f);
 		m_blockModel->getTransform().setScale(scale);
-		m_middle = DirectX::SimpleMath::Vector3((m_level->getGridWidth() * m_level->DEFAULT_BLOCKSIZE) / 2.f - level->DEFAULT_BLOCKSIZE / 2.f, (m_level->getGridHeight() * m_level->DEFAULT_BLOCKSIZE) / 2.f - level->DEFAULT_BLOCKSIZE / 2.f, 3.f);
+		m_middle = DirectX::SimpleMath::Vector3((m_level->getGridWidth() * m_level->DEFAULT_BLOCKSIZE) / 2.f, (m_level->getGridHeight() * m_level->DEFAULT_BLOCKSIZE) / 2.f - level->DEFAULT_BLOCKSIZE / 2.f, 3.f);
 		for (int i = 0; i < m_timeBlocks.size(); i++) {
 			m_timeBlocks[i] = std::make_unique<AnimatedObject>(m_blockModel);
 			
-			float x = cos(i * paj + PI / 2.f) * 2.5f * scale + m_middle.x;
-			float y = sin(i * paj + PI / 2.f) * 2.5f * scale + m_middle.y;
+			float numOfTeams = float(m_gamemode->getScore().size());
+			float x = cos(i * paj + PI / 2.f) * 2.5 * numOfTeams * scale + m_middle.x;
+			float y = sin(i * paj + PI / 2.f) * 5.f * scale + m_middle.y;
 			m_timeBlocks[i]->setPosition(m_middle);
 			DirectX::SimpleMath::Vector3 rot = DirectX::SimpleMath::Vector3(y - m_middle.y, -(x - m_middle.x), 0.f);
 			rot.Normalize();
 
 			m_timeBlocks[i]->getTransform().rotateAroundZ(atan2(rot.y, rot.x));
 
-			m_timeBlocks[i]->setTargetPos(DirectX::SimpleMath::Vector3(x, y, 3.f * scale), 3.f);
+			m_timeBlocks[i]->setTargetPos(DirectX::SimpleMath::Vector3(x, y, 15.f * scale), 3.f);
 
 			m_toRemove[i] = false;
 		}
@@ -69,7 +72,8 @@ void TimeVisualization::update(float dt) {
 			m_toRemove[i] = true;
 			DirectX::SimpleMath::Vector3 dir = m_timeBlocks[i]->getTransform().getTranslation() - m_middle;
 			DirectX::SimpleMath::Vector3 targetPos = dir * (max(m_middle.x / 2.f, m_middle.y / 2.f));
-			targetPos.z = 0.f;
+			targetPos = m_timeBlocks[i]->getTransform().getTranslation();
+			targetPos.z = 1000.f;
 			m_timeBlocks[i]->setTargetPos(targetPos, 4.f);
 		}
 	}
